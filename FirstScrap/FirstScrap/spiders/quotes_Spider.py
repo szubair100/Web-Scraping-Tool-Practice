906f99063cff5bfac0f21da232175cbd5520aaa8
szubair100 <szubair100@gmail.com>
2017-11-01 00:19:38 -0400

    My first start, it did not create new file folder in my spider

---
+++ FirstScrap/FirstScrap/spiders/quotes_Spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider (scrapy.Spider):
+	name = "quote1"
+	
+def start_requests(self):
+	start_urls = [
+		'http://quotes.toscrape.com/page/1/',
+		'http://quotes.toscrape.com/page/2/'
+	]
+
+	for url in urls:
+		yield scrapy.Request(url = url, callback = self.parse)
+
+def parse (self, response):
+	page = response.url.split("/")[-2]
+	filename = 'quotes-%s.html' % page
+	with open(filename, 'wb') as f:
+		f.write(response.body)
+	self.log('Saved file %s' % filename)
